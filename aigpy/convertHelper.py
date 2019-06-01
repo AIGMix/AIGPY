ab19837db8ea8c178d23f31a8534492ac35f05a1
yaronh <392309221@qq.com>
2019-06-01 17:48:52 +0800

    update

--- aigpy/convertHelper.py
+++ aigpy/convertHelper.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 def convertStorageUnit(num, srcUnit, desUnit):
     """
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'aigpy',
-    version='2019.5.9.2',
+    version='2019.6.1.0',
     license = "MIT Licence",
     description = "Python Common Tool",
 
