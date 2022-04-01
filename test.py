3c2fc3a9f29157b609f2598345eab2ca2ac721f1
Yaronzz <yaronhuang@foxmail.com>
2022-04-01 14:10:06 +0800

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :  test.py
+@Date    :  2022/04/01
+@Author  :  Yaronzz
+@Version :  1.0
+@Contact :  yaronhuang@foxmail.com
+@Desc    :  
+"""
+
+
+if __name__ == '__main__':
+    print('hello world.')
